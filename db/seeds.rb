221a959715163d85053ae41d9ca6e394e0549b16
Davender Singh <dsingh11@gmail.com>
2014-05-18 02:05:20 +0530

    seed file modified

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+statements = Statement.create([
+{statement: 'What color are car tires?'},
+{statement: 'What is 2x2?'},
+{statement: 'Who was the first president of america?'}
+])
+
+options = Option.create([
+{statement_id: 1, option: 'Red'},
+{statement_id: 1, option: 'Green'},
+{statement_id: 1, option: 'Black'},
+{statement_id: 1, option: 'White'},
+{statement_id: 2, option: '1'},
+{statement_id: 2, option: '2'},
+{statement_id: 2, option: '3'},
+{statement_id: 2, option: '4'},
+{statement_id: 3, option: 'George washington'},
+{statement_id: 3, option: 'Abraham lincoln'},
+{statement_id: 3, option: 'Bill clinton'},
+{statement_id: 3, option: 'Barack obama'}
+])
